5669e43c98b03dbc6a27aebea6809aa610b54956
sbrielma <scottbrielmaier@hasd.org>
2021-11-07 13:40:16 -0600

    Quick feedback

--- TangleOfStrings/src/tangleOfStrings/TangleOfStrings.java
+++ TangleOfStrings/src/tangleOfStrings/TangleOfStrings.java
@@ -9,6 +9,7 @@ Have fun with this tangle of strings! P.S this computer has feelings.
 */
 public class TangleOfStrings {	
 public static void main(String[] args) {
+		// This looks good - TS
 		System.out.println("Tangle of Strings");
 		Password();
 	}
